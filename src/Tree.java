e5103da72a086dfdf5fd68cef2ec43ec7e686f85
Stepan_Suprunov <ipsuprunovso@mail.ru>
2024-01-19 19:05:34 +0300

    Initial commit

---
+++ src/Tree.java
@@ -0,0 +1,24 @@
+public class Tree<V extends Comparable<V>> {
+    private Node root;
+
+    private class Node {
+        private V value;
+        private Node left;
+        private Node right;
+    }
+
+    public boolean contains(V value) {
+        Node node = root;
+        while (node != null) {
+            if (node.value.equals(value)) {
+                return true;
+            }
+            if (node.value.compareTo(value) > 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return false;
+    }
+}
